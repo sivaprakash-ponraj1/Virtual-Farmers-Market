48f53d80621f017ec138feb96a6c5ef0d2b53a86
sivaprakash-ponraj1 <sivaprakashponraj@gmail.com>
2025-03-27 19:17:24 +0530

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("Welcome to the Virtual Farmers Market!");
-});
+function searchItems() {
+    let input = document.getElementById("searchBar").value.toLowerCase();
+    let items = document.querySelectorAll(".item");
+
+    items.forEach(item => {
+        let name = item.getAttribute("data-name").toLowerCase();
+        if (name.includes(input)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
